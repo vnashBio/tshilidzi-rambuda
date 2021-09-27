07bde559f0bd16fb045510a1177c9cfb0bea0f9f
vnashBio <rambudatshilidzi1@gmail.com>
2021-09-27 12:59:52 +0200

    second commit

--- server.js
+++ server.js
@@ -12,10 +12,10 @@ app.use("/", router);
 //for production
 if(process.env.NODE_ENV === 'production'){
   //set a static folder
-  app.use(express.static('client/build'));
+  app.use(express.static('build'));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
   });
 }
 
